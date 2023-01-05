768363c86d72945fd566f2628a9da2c246ec44ba
Tabu Hana <taboodog@hotmail.com>
2023-01-05 14:02:00 -0800

    app moved over

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import SavedGame from './pages/SavedGame';
+import SearchGame from './pages/SearchGame';
+
+// Construct our main GraphQL API endpoint
+const httpLink = createHttpLink({
+	uri: 'http://localhost:5000/graphql',
+});
+
+// Construct request middleware that will attach the JWT token to every request as an `authorization` header
+const authLink = setContext((_, { headers }) => {
+	// get the authentication token from local storage if it exists
+	const token = localStorage.getItem('id_token');
+	// return the headers to the context so httpLink can read them
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : '',
+		},
+	};
+});
+
+const client = new ApolloClient({
+	// Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<ApolloProvider client={client}>
+			<Router>
+				<>
+					<Navbar />
+					<Routes>
+						<Route path='/' element={<SearchGame />} />
+						<Route path='/saved' element={<SavedGame />} />
+					</Routes>
+				</>
+			</Router>
+		</ApolloProvider>
+	);
 }
 
-export default App
+export default App;
