64195c9e4ac728ad621d15231a9e96b35ca72e33
Tabu Hana <taboodog@hotmail.com>
2023-01-05 13:48:51 -0800

    connection

---
+++ config/connection.js
@@ -0,0 +1,27 @@
+// const mongoose = require('mongoose');
+
+
+// const connectDB = async () => {
+//   try {
+//     mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mglDB', {
+//       useNewUrlParser: true,
+//       // useUnifiedTopology: true,
+//       // useCreateIndex: true,
+//       // useFindAndModify: false
+//     });
+//     console.log('DB connection')
+//   } catch (error) {
+//     console.log(`Error: ${ error.message }`.red.underline.bold);
+//     process.exit(1);
+//   }
+// };
+
+
+// module.exports = connectDB;
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/mglDB');
+
+module.exports = mongoose.connection;
+
