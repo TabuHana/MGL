125f581d46c64b8b75d87a0838b25d30b79ba59e
Tabu Hana <taboodog@hotmail.com>
2023-01-05 13:53:08 -0800

    schemas

---
+++ schemas/typeDefs.js
@@ -0,0 +1,49 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    gameCount: Int
+    savedGames: [game]
+  }
+
+  type game {
+    gameId: ID!
+    creator: [String]
+    description: String
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+
+  input GameInput {
+
+    creator: [String]
+    description: String!
+    gameId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveGame(creator: String!, description: String!, gameId: String!, image: String!, title: String!): User
+    removeGame(gameId: ID!): User
+  }
+`;
+
+module.exports = typeDefs;
