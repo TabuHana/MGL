dd24cd1836d6a1ae2cec4a8c6d0c3ccf5fed2948
Tabu Hana <taboodog@hotmail.com>
2023-01-05 13:53:13 -0800

    server

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
-console.log(123)
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+const connectDB = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+let server = null;
+const startServer = async () => {
+  server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: authMiddleware,
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+startServer();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+connectDB.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${ PORT }!`);
+    console.log(`Use GraphQL at http://localhost:${ PORT }${ server.graphqlPath }`);
+  });
+});
